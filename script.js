b72415939de1588b2a5d745ff83cf12f46cd867f
Sarav <Saravjeetofficial@gmail.com>
2024-05-24 11:19:08 +0530

    added javascript, data storage functions and eventlisteners

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value===''){
+        alert("You must enter something!");
+    }
+    else{
+        let li= document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        //adding icon within span tag
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";  //cross icon
+        li.appendChild(span);
+    }
+    inputBox.value= "";
+    saveData();
+}
+
+//js for click function
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove(); //li will be removed so task is deleted
+        saveData();
+    }
+}, false);
+
+//storing data
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+//display data whenever we open the website again
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
